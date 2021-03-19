bc7b8ed841402cdc9601b438d2df25b8caa34c47
nataliaortizvi <natiorvi00@gmail.com>
2021-03-19 04:52:29 -0500

    interaction almost done

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
+//GALLERY
 const current = document.querySelector('.gallery__current');
 const smalls = document.querySelectorAll('.gallery__small');
 
-
+//GALLERY FUNCTION
 for(let i = 0; i < smalls.length; i++){
     const small = smalls[i];
 
@@ -14,3 +15,52 @@ for(let i = 0; i < smalls.length; i++){
 
     if(i == 0) handleSmallClick(); 
 }
+
+
+
+//INTERACTION
+const radioButton = document.querySelectorAll('.views__button');
+const mainImage = document.querySelector('.interaction__img');
+
+//INTERACTION FUNCTION
+
+function handleForEach (elem, i) {
+
+    function handleCheck () {
+        console.log(elem,i)
+        switch (i) {
+            case 0:
+                //front
+                mainImage.classList.remove('interaction__img--side')
+                mainImage.classList.remove('interaction__img--back')
+                mainImage.classList.remove('interaction__img--down')
+                mainImage.classList.add('interaction__img--front')
+                break;
+            case 1:
+                //side
+                mainImage.classList.remove('interaction__img--front')
+                mainImage.classList.remove('interaction__img--back')
+                mainImage.classList.remove('interaction__img--down')
+                mainImage.classList.add('interaction__img--side')
+                break;
+            case 2:
+                //down
+                mainImage.classList.remove('interaction__img--front')
+                mainImage.classList.remove('interaction__img--back')
+                mainImage.classList.remove('interaction__img--side')
+                mainImage.classList.add('interaction__img--down')
+                break;
+            case 2:
+                //back
+                mainImage.classList.remove('interaction__img--front')
+                mainImage.classList.remove('interaction__img--side')
+                mainImage.classList.remove('interaction__img--down')
+                mainImage.classList.add('interaction__img--back')
+                break;
+        }
+    }
+
+    elem.addEventListener('click', handleCheck);
+}
+
+radioButton.forEach(handleForEach);
