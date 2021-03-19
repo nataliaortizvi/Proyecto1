e53fee7264cd1ac2acbe7d9a99c659fe97b71c57
nataliaortizvi <natiorvi00@gmail.com>
2021-03-18 22:06:06 -0500

    gallery

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+const current = document.querySelector('.gallery__current');
+const smalls = document.querySelectorAll('.gallery__small');
+
+
+for(let i = 0; i < smalls.length; i++){
+    const small = smalls[i];
+
+    function handleSmallClick () {
+        const smallSrc = small.getAttribute('src');
+        current.setAttribute('src', smallSrc);
+    }
+    
+    small.addEventListener('click', handleSmallClick);
+}
